e591faee231dea31477e46f98f660cb6266b9a73
Pu Xingyu <pu.stshine@gmail.com>
2015-04-29 23:23:13 +0800

    Helm tweaks, make helm show more visible space

--- init.el
+++ init.el
@@ -137,14 +137,20 @@
 (when (executable-find "curl")
   (setq helm-google-suggest-use-curl-p t))
 
-(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+(setq helm-display-header-line              nil
+      helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
       helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t)
 
+(set-face-attribute 'helm-source-header nil :height 0.1)
+
 (helm-mode 1)
+(helm-adaptive-mode 1)
+;;(setq helm-autoresize-max-height 50)
+;;(helm-autoresize-mode 1)
 
 ;;; intergrate projectile with helm.
 (require 'helm-projectile)
