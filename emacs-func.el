150f3242b1f83552ea18a1af4da1254700cd584e
Pu Xingyu <pu.stshine@gmail.com>
2016-01-09 11:17:12 +0800

    Function to copy buffer file name to clipboard
    A function from the Prelude configuration.

--- emacs-func.el
+++ emacs-func.el
@@ -142,5 +142,15 @@ Switch projects and subprojects from NEXT back to TODO"
   (when (string-match "^MATCH \\([^,]+\\),\\([^,]+\\),\\([^,]+\\),\\([^,]+\\).*$" line)
 	(xref-make-file-location  (match-string 4 line) (string-to-number (match-string 2 line)) (string-to-number (match-string 3 line)))))
 
+;;; -----------------------------------------------
+(defun prelude-copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
 
 ;;; emacs-func.el ends here
