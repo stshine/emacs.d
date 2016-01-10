93f26da1705348c58b5f1fd0a47f43e04e133ffd
Pu Xingyu <pu.stshine@gmail.com>
2016-01-10 18:37:47 +0800

    Initial haskell language setup

--- init.el
+++ init.el
@@ -429,6 +429,11 @@
 (add-hook 'cider-mode-hook #'eldoc-mode)
 
 
+;; ------------------------ Haskell --------------------------
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+;; (add-hook 'haskell-mode-hook 'stack-mode)
+;; (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook 'haskell-doc-mode)
 
 ;; -------------------- Chinese Setup -------------------------------
 (require 'chinese-pyim)
