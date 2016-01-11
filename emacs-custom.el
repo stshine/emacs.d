f0df29c9fde4781e6ef9e48ea983549bde9b57c1
Pu Xingyu <pu.stshine@gmail.com>
2016-01-11 22:15:33 +0800

    use-package setup for helm
    Some of the settings comes from spacemacs.

--- emacs-custom.el
+++ emacs-custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
     (racket-mode magit paredit paren-face moe-theme solarized-theme cider company-go go-projectile elpy spaceline spacemacs-theme which-key imenu-list ace-link ace-window company-auctex flycheck helm-projectile use-package))))
