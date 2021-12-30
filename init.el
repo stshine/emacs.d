cbf230d84913c9d6e50e13708f4da016a568d69d
Pu Xingyu <pu.stshine@gmail.com>
2021-12-30 16:41:04 +0800

    Remove company-lsp
    This package is obsolete.

--- init.el
+++ init.el
@@ -543,9 +543,7 @@ directories."
   :hook ((js2-mode python-mode rust-mode typescript-mode) . lsp)
   :diminish lsp-mode)
 
-(use-package company-lsp)
 (use-package lsp-ui :commands lsp-ui-mode)
-(use-package company-lsp :commands company-lsp)
 (use-package helm-lsp :commands helm-lsp-workspace-symbol)
 ;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 ;; optionally if you want to use debugger
